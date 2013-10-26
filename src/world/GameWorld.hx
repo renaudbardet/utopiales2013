7d5cda13949e4ff1b7cdaf987bc2137c17498b76
Samuel Bouchet <samuel.bouchet@gmail.com>
2013-10-26 12:39:21 +0200

    Game over

--- src/world/GameWorld.hx
+++ src/world/GameWorld.hx
@@ -56,6 +56,7 @@ class GameWorld extends Scene
 
 	private var hero : Hero ;
 	private var chrono:Label;
+	private var gameover:Label;
 	
 	private var runs : List<Run> ;
 	private var currentRun : Run ;
@@ -63,6 +64,8 @@ class GameWorld extends Scene
 	private var turn : Int ; // turn in the current run
 	private var inTime : Int ; // ms since turn start
 	private var currentMove : Option<Direction> ;
+	
+	private var gameEnd:Bool = false;
 	
 
 	public function new()
@@ -81,85 +84,95 @@ class GameWorld extends Scene
 		if (Input.pressed(Key.ESCAPE)) {
 			HXP.scene = WelcomeWorld.instance;
 		}
+		
+		if(!gameEnd){
 
-		inTime += Std.int( 1000/HXP.frameRate );
+			inTime += Std.int( 1000/HXP.frameRate );
 
-		// interpolate hero position
-		var prevFrame = currentRun.record.get(turn) ;
-		var nextX = prevFrame.x ;
-		var nextY = prevFrame.y ;
-		var currentDir = Up ;
-		var isMoving = true ;
-		switch ( currentMove ) {
-			case None:
-				currentDir = prevFrame.dir ;
-				isMoving = false ;
-			case Some( Up )		:
-				nextY -= moveSpanY ;
-				currentDir = Up ;
-			case Some( Down )	:
-				nextY += moveSpanY ;
-				currentDir = Down ;
-			case Some( Left )	:
-				nextX -= moveSpanX ;
-				currentDir = Left ;
-			case Some( Right )	:
-				nextX += moveSpanX ;
-				currentDir = Right ;
-		}
-		hero.x = moveTween( inTime, TURN_DURATION, prevFrame.x, nextX ) ;
-		hero.y = moveTween( inTime, TURN_DURATION, prevFrame.y, nextY ) ;
-		hero.play( currentDir, isMoving ) ;
+			// interpolate hero position
+			var prevFrame = currentRun.record.get(turn) ;
+			var nextX = prevFrame.x ;
+			var nextY = prevFrame.y ;
+			var currentDir = Up ;
+			var isMoving = true ;
+			switch ( currentMove ) {
+				case None:
+					currentDir = prevFrame.dir ;
+					isMoving = false ;
+				case Some( Up )		:
+					nextY -= moveSpanY ;
+					currentDir = Up ;
+				case Some( Down )	:
+					nextY += moveSpanY ;
+					currentDir = Down ;
+				case Some( Left )	:
+					nextX -= moveSpanX ;
+					currentDir = Left ;
+				case Some( Right )	:
+					nextX += moveSpanX ;
+					currentDir = Right ;
+			}
+			hero.x = moveTween( inTime, TURN_DURATION, prevFrame.x, nextX ) ;
+			hero.y = moveTween( inTime, TURN_DURATION, prevFrame.y, nextY ) ;
+			hero.play( currentDir, isMoving ) ;
 
-		// pilot ghosts
-		for( r in runs )
-		{
-			prevFrame = r.record.get( turn ) ;
-			var nextFrame = r.record.get( turn + 1 ) ;
-			r.ghost.x = moveTween( inTime, TURN_DURATION, prevFrame.x, nextFrame.x ) ;
-			r.ghost.y = moveTween( inTime, TURN_DURATION, prevFrame.y, nextFrame.y ) ;
-			r.ghost.play( nextFrame.dir, prevFrame.x != nextFrame.x || prevFrame.y != nextFrame.y ) ;
-		}
-		// turn advancement
-		if( inTime > TURN_DURATION )
-		{
-			
-			nextTurn() ;
-		}		var runDuration = (TURNS_PER_RUN * TURN_DURATION);
-		var remainingTime:Float = Math.ceil((runDuration - turn * TURN_DURATION) / 1000);
-		var remainingTimeStr:String = Std.string(remainingTime);
-		if (remainingTimeStr.indexOf(".") < 0) {
-			remainingTimeStr = remainingTimeStr + ".0";
-		}
-		trace(remainingTimeStr);
-		chrono.text = Std.string(remainingTime);
-		if (remainingTime > 3) {
-			chrono.color = 0xFFFFFF;
-		} else {
-			chrono.color = 0xFF3E3E;
+			// pilot ghosts
+			for( r in runs )
+			{
+				prevFrame = r.record.get( turn ) ;
+				var nextFrame = r.record.get( turn + 1 ) ;
+				r.ghost.x = moveTween( inTime, TURN_DURATION, prevFrame.x, nextFrame.x ) ;
+				r.ghost.y = moveTween( inTime, TURN_DURATION, prevFrame.y, nextFrame.y ) ;
+				r.ghost.play( nextFrame.dir, prevFrame.x != nextFrame.x || prevFrame.y != nextFrame.y ) ;
+			}
+			// turn advancement
+			if( inTime > TURN_DURATION )
+			{
+				
+				nextTurn() ;
+			}		var runDuration = (TURNS_PER_RUN * TURN_DURATION);
+			var remainingTime:Float = Math.ceil((runDuration - turn * TURN_DURATION) / 1000);
+			var remainingTimeStr:String = Std.string(remainingTime);
+			if (remainingTimeStr.indexOf(".") < 0) {
+				remainingTimeStr = remainingTimeStr + ".0";
+			}
+			trace(remainingTimeStr);
+			chrono.text = Std.string(remainingTime);
+			if (remainingTime > 3) {
+				chrono.color = 0xFFFFFF;
+			} else {
+				chrono.color = 0xFF3E3E;
+			}
 		}
 	}
 
 	private function nextTurn()
 	{
+		// condition de fin
+		if (hero.collide("vision", hero.x, hero.y) != null) {
+			gameEnd = true;
+			add(gameover);
+		}
+		
+		if(!gameEnd){
+			++turn ;
+			inTime = inTime % TURN_DURATION ;
 
-		++turn ;
-		inTime = inTime % TURN_DURATION ;
+			record() ;
 
-		record() ;
+			currentMove = None ;
+			if (Input.check("up"))
+				currentMove = Some(Up) ;
+			else if (Input.check("down"))
+				currentMove = Some(Down) ;
+			else if (Input.check("left"))
+				currentMove = Some(Left) ;
+			else if (Input.check("right"))
+				currentMove = Some(Right) ;
 
-		currentMove = None ;
-		if (Input.check("up"))
-			currentMove = Some(Up) ;
-		else if (Input.check("down"))
-			currentMove = Some(Down) ;
-		else if (Input.check("left"))
-			currentMove = Some(Left) ;
-		else if (Input.check("right"))
-			currentMove = Some(Right) ;
-
-		if( turn >= TURNS_PER_RUN )
-			timeJump() ;
+			if( turn >= TURNS_PER_RUN )
+				timeJump() ;
+		}
 
 	}
 	
@@ -168,6 +181,9 @@ class GameWorld extends Scene
 		// création des objets du niveau
 		hero = new Hero();
 		chrono = new Label();
+		gameover = new Label("Paradoxe !");
+		gameover.x = HXP.screen.width / 2 - gameover.width / 2;
+		gameover.y = HXP.screen.height / 2 - gameover.height / 2;
 		
 		// positionnemetn des élements d'interface
 		chrono.x = Math.round(HXP.screen.width/2 - 20);
